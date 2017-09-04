2f8f013b35e338b3788cafd5978c6ef26c2e4b56
Teddy <teddyivanov95@gmail.com>
2017-09-04 12:05:39 -0500

    Initial Commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Teodor Ivanov on 9/4/17.
+//  Copyright © 2017 Teodor Ivanov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
